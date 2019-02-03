ca909dea47bf3ba25ff666eff2f770b98a231268
kstandbridge <prepotency@gmail.com>
2019-02-03 16:34:49 +0000

    Logging wrapper

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Log.h"
 
 #ifdef HZ_PLATFORM_WINDOWS
 
@@ -6,6 +7,11 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+	
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
